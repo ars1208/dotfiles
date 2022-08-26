f4082f63627e06f123ac9f04f4a71580ccfcc82c
ars1208 <ara.kaz1208@gmail.com>
2022-08-26 14:36:17 +0900

    chore: フォントの設定を追加

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,8 @@
 local wezterm = require 'wezterm'
 return {
+  ----------------- fonts
+  font = wezterm.font('JetBrains Mono', { weight = 'Bold' }),
+  font_size = 14.0,
+  ----------------- window
   color_scheme = 'Afterglow',
 }
