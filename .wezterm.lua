32cdc0f88ccc0b71b9c20835549da0ed32aef120
ars1208 <ara.kaz1208@gmail.com>
2022-08-30 22:42:26 +0900

    chore: 入力時の設定を追加

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,8 @@
 local wezterm = require 'wezterm'
 return {
+  ----------------- input
+  use_ime = true,
+  ime_preedit_rendering = 'System',
   ----------------- fonts
   font = wezterm.font('JetBrains Mono', { weight = 'Bold' }),
   font_size = 14.0,
