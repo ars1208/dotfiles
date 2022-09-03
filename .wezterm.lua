7d596b88d45a93f92336906af04a8b56f062bab1
ars1208 <ara.kaz1208@gmail.com>
2022-09-03 21:25:15 +0900

    chore: windowサイズの修正を追加

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,6 +7,7 @@ return {
   font = wezterm.font('JetBrains Mono', { weight = 'Bold' }),
   font_size = 14.0,
   line_height = 1.1,
+  adjust_window_size_when_changing_font_size = false,
   ----------------- window
   color_scheme = "nord",
 }
