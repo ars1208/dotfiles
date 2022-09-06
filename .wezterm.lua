00e937d0cdd2981398e80a22e86a1fb213ff138d
ars1208 <ara.kaz1208@gmail.com>
2022-09-06 22:56:43 +0900

    chore: fontの記述を変更

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,9 @@ return {
   use_ime = true,
   ime_preedit_rendering = 'System',
   ----------------- fonts
-  font = wezterm.font('JetBrains Mono', { weight = 'Bold' }),
+  font = wezterm.font_with_fallback {
+    'JetBrains Mono',
+  },
   font_size = 14.0,
   line_height = 1.1,
   adjust_window_size_when_changing_font_size = false,
