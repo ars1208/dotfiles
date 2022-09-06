a97314a280c17c63b8989585bd9ee14ed93c68fd
ars1208 <ara.kaz1208@gmail.com>
2022-09-06 23:01:37 +0900

    chore: fontを追加

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,9 +5,10 @@ return {
   ime_preedit_rendering = 'System',
   ----------------- fonts
   font = wezterm.font_with_fallback {
+    'Cica',
     'JetBrains Mono',
   },
-  font_size = 14.0,
+  font_size = 18.0,
   line_height = 1.1,
   adjust_window_size_when_changing_font_size = false,
   ----------------- window
