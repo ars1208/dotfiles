391b72eb17824e802a4711c10cd7a70a098a6ac1
ars1208 <ara.kaz1208@gmail.com>
2024-11-13 15:20:02 +0900

    chore: add config for desktop pc

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,18 +1,28 @@
+-- Pull in the wezterm API
 local wezterm = require 'wezterm'
-return {
-  ----------------- input
-  use_ime = true,
-  ime_preedit_rendering = 'System',
-  ----------------- fonts
-  font = wezterm.font_with_fallback {
-    'Cica',
-    'JetBrains Mono',
-  },
-  font_size = 18.0,
-  line_height = 1.1,
-  adjust_window_size_when_changing_font_size = false,
-  enable_scroll_bar = true,
-  ----------------- window
-  color_scheme = "nord",
-  scrollback_lines = 20000,
+
+-- Hold the configuration
+local config = {}
+
+config.scrollback_lines = 9999999
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
+-- input
+config.use_ime = true
+config.ime_preedit_rendering = 'System'
+
+-- fonts
+config.font = wezterm.font_with_fallback {
+  'Cica',
+  'JetBrains Mono',
 }
+config.font_size = 18.0
+config.line_height = 1.1
+config.adjust_window_size_when_changing_font_size = false
+config.enable_scroll_bar = true
+
+-- window
+config.color_scheme = 'nord'
+config.window_background_opacity = 0.8
+
+return config
