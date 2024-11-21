5115f6676510c07a183ce311f6a2172fbec5de76
ars1208 <ara.kaz1208@gmail.com>
2024-11-21 22:45:00 +0900

    add: add config

--- .wezterm.lua
+++ .wezterm.lua
@@ -165,5 +165,7 @@ config.colors = {
 -- window
 config.color_scheme = 'nord'
 config.window_background_opacity = 0.8
+config.macos_window_background_blur = 20
+config.window_decorations = "RESIZE"
 
 return config
